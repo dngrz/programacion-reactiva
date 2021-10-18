67e47e248c9c49e79e7f082d8eb5d54727ee35a6
Clodoaldo Sánchez <clodoaldosanchez@gmail.com>
2021-10-18 16:46:26 -0500

    feat: hotStreamTest

--- src/test/java/pe/net/csweb/pruebas/programacionreactiva/HotAndColdStreamsTest.java
+++ src/test/java/pe/net/csweb/pruebas/programacionreactiva/HotAndColdStreamsTest.java
@@ -1,7 +1,11 @@
 package pe.net.csweb.pruebas.programacionreactiva;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.Test;
 
+import lombok.SneakyThrows;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 
 public class HotAndColdStreamsTest {
@@ -16,5 +20,21 @@ public class HotAndColdStreamsTest {
 		numeros.subscribe(entero -> System.out.println("Subscriber 2 = " + entero));
 		
 	}
-	
+
+	@SneakyThrows
+	@Test
+	void hotStreamTest() {
+		
+		Flux<Integer> numeros = Flux.range(1, 10).delayElements(Duration.ofMillis(1000));
+
+		ConnectableFlux<Integer> publisher = numeros.publish();
+		
+		publisher.connect();
+		
+		publisher.subscribe(entero -> System.out.println("Subscriber 1 = " + entero));
+		Thread.sleep(4000);
+		publisher.subscribe(entero -> System.out.println("Subscriber 2 = " + entero));
+		
+	}
+
 }
