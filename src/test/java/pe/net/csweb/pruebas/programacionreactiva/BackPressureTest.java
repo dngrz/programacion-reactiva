d3bdd3dee006910e245c20318acf0c1c5e520132
Clodoaldo Sánchez <clodoaldosanchez@gmail.com>
2021-10-18 00:42:02 -0500

    feat: test - BackPressureTest

---
+++ src/test/java/pe/net/csweb/pruebas/programacionreactiva/BackPressureTest.java
@@ -0,0 +1,47 @@
+package pe.net.csweb.pruebas.programacionreactiva;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+public class BackPressureTest {
+
+	@Test
+	void testBackPressure() {
+
+		Flux<Integer> numbers = Flux.range(1, 100).log();
+
+		// numbers.subscribe(entero -> System.out.println(entero));
+
+		numbers.subscribe(new BaseSubscriber<Integer>() {
+
+			@Override
+			protected void hookOnSubscribe(Subscription subscription) {
+				request(3);
+			}
+
+			@Override
+			protected void hookOnNext(Integer value) {
+				System.out.println("valor = " + value);
+
+				if (value == 3)
+					cancel();
+
+			}
+
+			@Override
+			protected void hookOnComplete() {
+				System.out.println("Completed!!");
+			}
+
+			@Override
+			protected void hookOnCancel() {
+				super.hookOnCancel();
+			}
+
+		});
+	}
+
+}
