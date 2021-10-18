8fda1af8b257ee227b11f69aa394a77e13d44d3c
Clodoaldo Sánchez <clodoaldosanchez@gmail.com>
2021-10-18 14:43:59 -0500

    feat: onBackpressureError()

--- src/test/java/pe/net/csweb/pruebas/programacionreactiva/BackPressureTest.java
+++ src/test/java/pe/net/csweb/pruebas/programacionreactiva/BackPressureTest.java
@@ -119,4 +119,44 @@ public class BackPressureTest {
 			});
 	}
 
+	@Test
+	void testBackPressureError() {
+
+		Flux<Integer> numbers = Flux.range(1, 100).log();
+
+		// numbers.subscribe(entero -> System.out.println(entero));
+
+		numbers
+				.onBackpressureError()
+				.subscribe(new BaseSubscriber<Integer>() {
+
+				@Override
+				protected void hookOnSubscribe(Subscription subscription) {
+					request(3);
+				}
+	
+				@Override
+				protected void hookOnNext(Integer value) {
+					System.out.println("valor = " + value);
+	
+					if (value == 3) hookOnCancel();
+	
+				}
+				
+				@Override
+					protected void hookOnError(Throwable throwable) {
+						System.out.println("throwable = " + throwable);
+					}
+	
+				@Override
+				protected void hookOnComplete() {
+					System.out.println("Completed!!");
+				}
+	
+				@Override
+				protected void hookOnCancel() {
+					super.hookOnCancel();
+				}
+			});
+	}
 }
